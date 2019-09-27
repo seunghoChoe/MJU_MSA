a7225e44560025888bbbec14fea87510fd40cb04
seunghoChoe <seungho93@mju.ac.kr>
2019-09-27 21:06:38 +0900

    "second push and commit"

--- spring-Interceptor-test/src/main/java/com/spring/test/HomeController.java
+++ spring-Interceptor-test/src/main/java/com/spring/test/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("text", "Hello Maven Jenkins Git ..");
 		return new String("home");
 	}
 	
