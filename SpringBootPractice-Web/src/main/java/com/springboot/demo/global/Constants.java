161081a82f0857de794464708e3f02cac4930e4b
seunghoChoe <seungho93@mju.ac.kr>
2019-10-17 11:05:42 +0900

    web -service ip 수정

--- SpringBootPractice-Web/src/main/java/com/springboot/demo/global/Constants.java
+++ SpringBootPractice-Web/src/main/java/com/springboot/demo/global/Constants.java
@@ -3,6 +3,6 @@ package com.springboot.demo.global;
 public class Constants {
 	
 	public static final String API_GATEWAY_IPADDRESS = "52.78.148.181";
-	public static final String API_GATEWAY_PORTNUMBER = "8892";
+	public static final String API_GATEWAY_PORTNUMBER = "8080";
 
 }
