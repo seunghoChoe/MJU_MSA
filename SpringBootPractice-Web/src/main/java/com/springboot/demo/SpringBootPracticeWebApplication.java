288f44f7098be36e33983efd4dc388f783f8511d
jaehason <son0618@mju.ac.kr>
2019-10-30 14:11:48 +0900

    에러 뷰 테스트

--- SpringBootPractice-Web/src/main/java/com/springboot/demo/SpringBootPracticeWebApplication.java
+++ SpringBootPractice-Web/src/main/java/com/springboot/demo/SpringBootPracticeWebApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-
 @SpringBootApplication
 public class SpringBootPracticeWebApplication extends SpringBootServletInitializer {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticeWebApplication.class, args);
@@ -18,7 +15,5 @@ public class SpringBootPracticeWebApplication extends SpringBootServletInitializ
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringBootPracticeWebApplication.class);
-		}
-
-
+	}
 }
